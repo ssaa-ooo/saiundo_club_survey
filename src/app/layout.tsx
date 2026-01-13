95cb2c86908626e89758e90d8f57922635413cff
ssaa-ooo <ss@ssaa.ooo>
2026-01-13 23:33:58 +0900

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "彩雲堂倶楽部アンケート",
+  description: "彩雲堂の接客向上アンケートシステム",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
